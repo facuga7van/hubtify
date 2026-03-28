b0e507ed15fb151559dff006f3f5124fa6000d6a
facuga7van <70443572+facuga7van@users.noreply.github.com>
2026-03-28 19:39:49 -0300

    chore: bump version to 0.4.0

--- forge.config.ts
+++ forge.config.ts
@@ -57,7 +57,7 @@ const config: ForgeConfig = {
     },
     icon: path.resolve(__dirname, 'assets/icon'),
     extraResource: ['./assets/icon.ico'],
-    appVersion: '0.3.4',
+    appVersion: '0.4.0',
     appCopyright: 'Hubtify',
     win32metadata: {
       CompanyName: 'Hubtify',
